5ab9541e33377d20393f3a7b719663f465543db8
SUYOG PATIL <80512884+suyog73@users.noreply.github.com>
2021-07-05 09:36:03 +0530

    OK

---
+++ index.js
@@ -0,0 +1,11 @@
+// day button slide up downn!
+
+    $("button.weekbtn1").on("click",function(){
+      $("div.dayslide1").slideToggle();
+    });
+  $("button.weekbtn2").on("click",function(){
+    $("div.dayslide2").slideToggle();
+  });
+  $("button.weekbtn3").on("click",function(){
+    $("div.dayslide3").slideToggle();
+  });
